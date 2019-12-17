57854a49b964f8197a3ec8838552f5bd9a9931eb
Sridhar Sarnobat <ss401533@gmail.com>
2019-12-17 13:03:18 -0800

    Minor changes (untested)

---
+++ 3_helloworld_make_multifile/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "helper.c"
+
+int main() {
+	printf("Hello World\n");
+	helper();
+	return 0;
+}
---
+++ 3_helloworld_make_multifile/helper.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void helper() {
+	printf("helper()\n");
+}
