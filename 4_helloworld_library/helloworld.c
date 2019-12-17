4595dfee7083450bf1e6a50964d96bd991328394
ssarnobat <ss401533@gmail.com>
2019-12-17 13:51:57 -0800

    Minor changes (untested)

--- 4_helloworld_library/helloworld.c
+++ 4_helloworld_library/helloworld.c
@@ -5,21 +5,21 @@ int main() {
 	 char * string = "{\"name\" : \"joys of programming\"}";
 	  json_object * jobj = json_tokener_parse(string);
 	   enum json_type type = json_object_get_type(jobj);
-	    printf("type: %d",type);
-	     switch (type) {
-		      case json_type_null: printf("json_type_nulln");
-					    break;
-					    case json_type_boolean: printf("json_type_booleann");
-								     break;
-								     case json_type_double: printf("json_type_doublen");
-											     break;
-											     case json_type_int: printf("json_type_intn");
-														  break;
-														  case json_type_object: printf("json_type_objectn");
-																	  break;
-																	  case json_type_array: printf("json_type_arrayn");
-																				 break;
-																				 case json_type_string: printf("json_type_stringn");
-																							 break;
-																							 }
+	   printf("type: %d\n",type);
+	   switch (type) {
+		   case json_type_null: printf("json_type_null\n");
+					break;
+		   case json_type_boolean: printf("json_type_boolean\n");
+					   break;
+		   case json_type_double: printf("json_type_double\n");
+					  break;
+		   case json_type_int: printf("json_type_intn");
+				       break;
+		   case json_type_object: printf("json_type_object\n");
+					  break;
+		   case json_type_array: printf("json_type_array\n");
+					 break;
+		   case json_type_string: printf("json_type_string\n");
+					  break;
+	   }
 }
--- 4_helloworld_library/run.sh
+++ 4_helloworld_library/run.sh
@@ -2,3 +2,5 @@ nm -D /lib/x86_64-linux-gnu/libjson-c.so.2
 
 make clean
 make helloworld
+
+./helloworld
