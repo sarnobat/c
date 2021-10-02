64fbf3ab32832c33e0cca559a5539fb2d9d7fa57
Sridhar Sarnobat <srsarnob@cisco.com>
2021-10-01 22:50:47 -0700

    Cron daily autocommit

--- 4_helloworld_library_static/mac/run.sh
+++ 4_helloworld_library_static/mac/run.sh
@@ -1,2 +1,9 @@
 gcc -c helloworld.c 	-I/opt/local/include
 gcc -o helloworld /opt/local/lib/libjson-c.dylib         -I/opt/local/lib        helloworld.o
+
+# To debug:
+
+# lldb ./helloworld
+# b main
+# run
+# s
