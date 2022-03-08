a6498349e6673d919f3eced861057d2ff46a2598
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-03-07 22:55:36 -0800

    Minor changes (untested)

--- 6_exif_library/run.sh
+++ 6_exif_library/run.sh
@@ -1,1 +1,3 @@
 gcc /Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/lib/libexif.12.dylib -I/Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/include/  photographer.c -o photographer
+
+find ~/trash/ -iname "*jpg" | xargs --delimiter '\n' --max-args=1 ./photographer | grep -v readable
