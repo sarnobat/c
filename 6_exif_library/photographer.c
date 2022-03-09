8c45e6f5fb88c5b095887b2500ac3d4fe59358cc
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-03-08 21:19:37 -0800

    Minor changes (untested)

--- 6_exif_library/photographer.c
+++ 6_exif_library/photographer.c
@@ -5,7 +5,7 @@
 #include <libexif/exif-data.h>
 
 /* Show the tag name and contents if the tag exists */
-static void show_tag(ExifData *ed, ExifIfd ifd, ExifTag tag)
+static void show_tag(ExifData *ed, ExifIfd ifd, ExifTag tag, char* filename)
 {
     /* See if this tag exists */
     ExifEntry *entry = exif_content_get_entry(ed->ifd[ifd],tag);
@@ -18,30 +18,39 @@ static void show_tag(ExifData *ed, ExifIfd ifd, ExifTag tag)
         /* Don't bother printing it if it's entirely blank */
 
         if (*buf) {
-            printf("%s:\t%s\n", exif_tag_get_name_in_ifd(tag,ifd), buf);
+            printf("%s\t%s\t%s\n", filename, exif_tag_get_name_in_ifd(tag,ifd), buf);
         }
     }
 }
 
 int main(int argc, char **argv)
 {
-    ExifData *ed;
+
     ExifEntry *entry;
 
     if (argc < 2) {
         printf("Usage: %s image.jpg\n", argv[0]);
         return 1;
     }
-
-    ed = exif_data_new_from_file(argv[1]);
+    ExifData *ed = exif_data_new_from_file(argv[1]);
     if (ed) {
-    	printf("%s\n", argv[1]);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MAKE);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MODEL);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_FILE_SOURCE);
-		show_tag(ed, EXIF_IFD_1, EXIF_TAG_FILE_SOURCE);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_DOCUMENT_NAME);
-		show_tag(ed, EXIF_IFD_1, EXIF_TAG_DOCUMENT_NAME);
+// //     	printf("%s\n", argv[1]);
+// 		ExifEntry *entry = exif_content_get_entry(ed->ifd[EXIF_IFD_0], EXIF_TAG_MAKE);
+// 		if (entry) {
+// 			char buf[1024];
+// 			/* Get the contents of the tag in human-readable form */
+// 			exif_entry_get_value(entry, buf, sizeof(buf));
+// 			/* Don't bother printing it if it's entirely blank */
+// 			if (*buf) {
+// // 				printf("%s:\t%s\t%s\n", exif_tag_get_name_in_ifd(EXIF_TAG_MAKE, EXIF_IFD_0), buf, argv[1]);
+// 			}
+// 		}
+		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MAKE, argv[1]);
+		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MODEL, argv[1]);
+		show_tag(ed, EXIF_IFD_0, EXIF_TAG_FILE_SOURCE, argv[1]);
+		show_tag(ed, EXIF_IFD_1, EXIF_TAG_FILE_SOURCE, argv[1]);
+		show_tag(ed, EXIF_IFD_0, EXIF_TAG_DOCUMENT_NAME, argv[1]);
+		show_tag(ed, EXIF_IFD_1, EXIF_TAG_DOCUMENT_NAME, argv[1]);
 		
 		/* Free the EXIF data */
 		exif_data_unref(ed);
