3b9e4b566abc382122ff74f959f77975ef02c92f
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-04-16 11:06:16 -0700

    Cron daily autocommit

--- 6_exif_library/photographer.c
+++ 6_exif_library/photographer.c
@@ -1,6 +1,7 @@
 // gcc -print-libgcc-file-name /Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/lib/libexif.12.dylib -I/Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/include/  photographer.c -o photographer
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <libexif/exif-data.h>
 
@@ -18,7 +19,7 @@ static void show_tag(ExifData *ed, ExifIfd ifd, ExifTag tag, char* filename)
         /* Don't bother printing it if it's entirely blank */
 
         if (*buf) {
-            printf("%s\t%s\t%s\n", filename, exif_tag_get_name_in_ifd(tag,ifd), buf);
+            printf("%-90s\t%-7s\t%s\n", filename, exif_tag_get_name_in_ifd(tag,ifd), buf);
         }
     }
 }
@@ -32,14 +33,46 @@ int main(int argc, char **argv)
         printf("Usage: %s image.jpg\n", argv[0]);
         return 1;
     }
+    char *filename = argv[1];
     ExifData *ed = exif_data_new_from_file(argv[1]);
     if (ed) {
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MAKE, argv[1]);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MODEL, argv[1]);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_FILE_SOURCE, argv[1]);
-		show_tag(ed, EXIF_IFD_1, EXIF_TAG_FILE_SOURCE, argv[1]);
-		show_tag(ed, EXIF_IFD_0, EXIF_TAG_DOCUMENT_NAME, argv[1]);
-		show_tag(ed, EXIF_IFD_1, EXIF_TAG_DOCUMENT_NAME, argv[1]);
+		char buf[1024];
+		char buf2[1024];
+
+		bool hasMake = false;
+		bool hasModel = false;
+// 		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MAKE, argv[1]);
+		ExifIfd ifd = EXIF_IFD_0;
+
+		{
+			ExifTag tag = EXIF_TAG_MAKE;
+			ExifEntry *entry = exif_content_get_entry(ed->ifd[ifd],tag);
+			if (entry) {
+				exif_entry_get_value(entry, buf, sizeof(buf));
+				if (*buf) {
+					hasMake = true;
+				}
+			}
+		}
+		{
+			ExifTag tag2 = EXIF_TAG_MODEL;
+			ExifEntry *entry2 = exif_content_get_entry(ed->ifd[ifd],tag2);
+			if (entry2) {		
+				exif_entry_get_value(entry2, buf2, sizeof(buf2));
+				if (*buf2) {
+					hasModel = true;
+				}
+			}
+		}
+		if (hasMake && hasModel) {
+			printf("%-90s\t%-7s\t%s\n", filename, buf, buf2);
+			//printf("%-90s\t%-7s\t%s%s\n", filename, exif_tag_get_name_in_ifd(tag,ifd), buf, buf2);
+		}
+//		show_tag(ed, EXIF_IFD_0, EXIF_TAG_MODEL, argv[1]);
+// 		show_tag(ed, EXIF_IFD_0, EXIF_TAG_FILE_SOURCE, argv[1]);
+// 		show_tag(ed, EXIF_IFD_1, EXIF_TAG_FILE_SOURCE, argv[1]);
+// 		show_tag(ed, EXIF_IFD_0, EXIF_TAG_DOCUMENT_NAME, argv[1]);
+// 		show_tag(ed, EXIF_IFD_1, EXIF_TAG_DOCUMENT_NAME, argv[1]);
 		
 		/* Free the EXIF data */
 		exif_data_unref(ed);
