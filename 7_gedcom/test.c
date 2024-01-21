61ee631791d951cdfd0526381c8eb8ded66c03a4
Sridhar Sarnobat <ss401533@gmail.com>
2024-01-21 15:51:04 -0800

    Minor changes (untested)

--- 7_gedcom/test.c
+++ 7_gedcom/test.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "gedcom.h"
 
-int main() {
+int main(int argc, char* argv[])
+{
+  Gedcom_err_mech mech = IMMED_FAIL;
+  int compat_enabled = 1;
+  int debug_level = 0;
+  int result      = 0;
+  char* outfilename = NULL;
+  char* file_name = NULL;
+
 	printf("hi\n");
 }
