0d222846e082b6db0fcae913e38b069181413543
Sridhar Sarnobat <ss401533@gmail.com>
2024-11-03 21:22:48 -0800

    Minor changes (untested) 2024-11-03

--- 5_helloworld_library.mac/6_exif_library/run.mac.sh
+++
@@ -1,4 +0,0 @@
-# gcc /Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/lib/libexif.12.dylib -I/Volumes/numerous/usr/local/homebrew/Cellar/libexif/0.6.23/include/  photographer.c -o photographer
-gcc /opt/homebrew/Cellar/libexif/0.6.24/lib/libexif.dylib	-I/opt/homebrew/Cellar/libexif/0.6.24/include photographer.c -o photographer.m1.osx
-
-find ~/trash/ -iname "*jpg" | xargs --delimiter '\n' --max-args=1 ./photographer.m1.osx | grep -v readable
---
+++ 5_helloworld_library.mac/run.mac.sh
@@ -0,0 +1,3 @@
+gcc /opt/homebrew/Cellar/libexif/0.6.24/lib/libexif.dylib	-I/opt/homebrew/Cellar/libexif/0.6.24/include helloworld.c -o helloworld.m1.osx
+
+find ~/trash/ -iname "*jpg" | xargs --delimiter '\n' --max-args=1 ./photographer.m1.osx | grep -v readable
