543ea1f95f281705beeb11353d50fdb9bc0c0748
sarnobat <srsarnob@cisco.com>
2025-06-02 16:01:26 -0400

    Create run.sh

---
+++ 9_bison_flex_parser/run.sh
@@ -0,0 +1,7 @@
+cat <<EOF
+For C code, see:
+/Volumes/git/github/2024/docker_images/bison_helloworld_cmake/src
+
+For a simpler makefile, look at the non-cmake version:
+/Volumes/git/github/2024/docker_images/bison_helloworld
+EOF
