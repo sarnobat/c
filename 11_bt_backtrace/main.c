58d9342944b8d3ed574e8b3cfb4f7ee313ae24ad
Sridhar Sarnobat <ss401533@gmail.com>
2025-08-06 01:10:56 -0400

    2025-08-06: main.c

--- 11_bt_backtrace/main.c
+++ 11_bt_backtrace/main.c
@@ -1,29 +1,24 @@
 
-#include <stdio.h>
-#include <stdlib.h> 
 #include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* Obtain a backtrace and print it to stdout. */
-void
-print_trace (void)
-{
+void print_trace(void) {
   void *array[10];
   char **strings;
   int size, i;
 
-  size = backtrace (array, 10);
-  strings = backtrace_symbols (array, size);
-  if (strings != NULL)
-  {
+  size = backtrace(array, 10);
+  strings = backtrace_symbols(array, size);
+  if (strings != NULL) {
 
-    printf ("Obtained %d stack frames.\n", size);
+    printf("Obtained %d stack frames.\n", size);
     for (i = 0; i < size; i++)
-      printf ("%s\n", strings[i]);
+      printf("%s\n", strings[i]);
   }
 
-  free (strings);
+  free(strings);
 }
 
-int main() {
-  print_trace();
-}
+int main() { print_trace(); }
