dff74f9d04aa29c18ad043a2db5bdb814bd7697f
Sridhar Sarnobat <ss401533@gmail.com>
2025-10-04 14:02:36 -0400

    2025-10-04: HelloWorld.jar Makefile hello hello.c

--- 15_java_embedded_jar/hello.c
+++ 15_java_embedded_jar/hello.c
@@ -1,26 +1,40 @@
 #include <jni.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    JavaVM *jvm;
-    JNIEnv *env;
-    JavaVMInitArgs vm_args;
-    JavaVMOption options[1];
+int main(void) {
+    JavaVM *jvm = NULL;
+    JNIEnv *env = NULL;
 
-    // Tell JVM to launch a JAR
-    options[0].optionString = "-jar HelloWorld.jar";
+    JavaVMOption opt;
+    opt.optionString = "-Djava.class.path=HelloWorld.jar";  // put your jar here
 
-    vm_args.version = JNI_VERSION_1_8;
-    vm_args.nOptions = 1;
-    vm_args.options = options;
-    vm_args.ignoreUnrecognized = JNI_FALSE;
+    JavaVMInitArgs args;
+    args.version = JNI_VERSION_1_8;
+    args.nOptions = 1;
+    args.options = &opt;
+    args.ignoreUnrecognized = JNI_FALSE;
 
-    if (JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args) < 0) {
+    if (JNI_CreateJavaVM(&jvm, (void**)&env, &args) != 0 || !env) {
         fprintf(stderr, "Failed to create JVM\n");
         return 1;
     }
 
-    // When you pass -jar, JVM executes it and then exits, so you don’t need to FindClass
-    (*jvm)->DestroyJavaVM(jvm);
+    // If your main class is in a package, use slashes, e.g. "com/acme/App"
+    jclass cls = (*env)->FindClass(env, "HelloWorld");
+    if (!cls) { (*env)->ExceptionDescribe(env); goto done; }
+
+    jmethodID mid = (*env)->GetStaticMethodID(env, cls, "main", "([Ljava/lang/String;)V");
+    if (!mid) { (*env)->ExceptionDescribe(env); goto done; }
+
+    // Create empty String[] args to pass to main
+    jclass strCls = (*env)->FindClass(env, "java/lang/String");
+    if (!strCls) { (*env)->ExceptionDescribe(env); goto done; }
+    jobjectArray emptyArgs = (*env)->NewObjectArray(env, 0, strCls, NULL);
+
+    (*env)->CallStaticVoidMethod(env, cls, mid, emptyArgs);
+    if ((*env)->ExceptionCheck(env)) { (*env)->ExceptionDescribe(env); }
+
+done:
+    if (jvm) (*jvm)->DestroyJavaVM(jvm);
     return 0;
 }
